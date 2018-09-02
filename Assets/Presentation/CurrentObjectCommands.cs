637bbb331ede8bd790caf64b8d2037e64c30cecf
alessio.langiu <alessio.langiu@gmail.com>
2018-09-02 16:43:03 +0100

    Draft for HistoryOfTheVirtualReality

---
+++ Assets/Presentation/CurrentObjectCommands.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CurrentObjectCommands : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            // Application.Quit() does not work in the editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
+        }
+    }
+}
